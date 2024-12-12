46885d358a4b1bffe7bc1298337feb02cb055211
oThommy <tvdhil@datho.com>
2024-12-12 03:42:54 +0100

    chore: add repo directory structure

---
+++ config/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parents[1]
+
+DATA_DIR = ROOT_DIR / 'data'
+RAW_DATA_DIR = DATA_DIR / 'raw'
+PROCESSED_DATA_DIR = DATA_DIR / 'processed'
+FINAL_DATA_DIR = DATA_DIR / 'final'
+
+MODELS_DIR = ROOT_DIR / 'models'
+
+REPORTS_DIR = ROOT_DIR / 'reports'
+FIGURES_DIR = REPORTS_DIR / 'figures'
